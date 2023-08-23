9c09b39efa9872df69d4081a0c102e24f2846156
BartoAlheiros <alheirosb@gmail.com>
2023-08-22 21:46:09 -0300

    salvando em arquivos

--- controller/registra_chamado.php
+++ controller/registra_chamado.php
@@ -1,7 +1,25 @@
 <?php
 
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
+
+    //trabalhando na montagem do texto
+    $titulo    = str_replace('#', '-', $_POST['titulo']);
+    $categoria = str_replace('#', '-', $_POST['categoria']);
+    $descricao = str_replace('#', '-', $_POST['descricao']);
+
+    // $texto = implode('#', $_POST);
+
+    $texto = $titulo . '#' . $categoria . '#' . $descricao;
+
+    //abrindo o arquivo
+    $arquivo = fopen('arquivo.hd', 'a');
+    //escrevendo o texto
+    fwrite($arquivo, $texto);
+    //fechando o arquivo
+    fclose($arquivo);
+
+    //echo $texto;
 
 ?>
